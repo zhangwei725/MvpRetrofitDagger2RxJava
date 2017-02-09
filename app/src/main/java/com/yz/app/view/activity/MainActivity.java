66b19544554f089c4f4e6037319087442fedd29e
wuxiaolong <wuxiaolong@xuexibao.cn>
2017-02-09 08:27:44 +0800

    我新开一个分支维护，发现点击事件不起作用，大家一起看看呗，我技术差，不要打我

--- app/src/main/java/com/yz/app/view/activity/MainActivity.java
+++ app/src/main/java/com/yz/app/view/activity/MainActivity.java
@@ -1,10 +1,17 @@
 package com.yz.app.view.activity;
 
 
+import android.content.Context;
+import android.support.design.widget.FloatingActionButton;
+import android.view.View;
+import android.widget.Toast;
+
 import com.yz.app.R;
 import com.yz.app.common.base.BaseActivity;
 
 public class MainActivity extends BaseActivity   {
+    private FloatingActionButton fab;
+    private Context mContext = this;
     @Override
     public int getLayoutId() {
         return R.layout.activity_main;
@@ -12,7 +19,13 @@ public class MainActivity extends BaseActivity   {
 
     @Override
     public void initView() {
-
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext,"你妈嗨",Toast.LENGTH_LONG);
+            }
+        });
     }
 
 }
